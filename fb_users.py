082937f8d7b4bca75773937836852d975ca2529b
Tankovod <Wotdimas@gmail.com>
2023-08-06 19:34:25 +0300

    simple commit

---
+++ fb_users.py
@@ -0,0 +1,19 @@
+from src.models import FbUsers
+
+users = [
+    ('xacraugihexou-5110@yopmail.com', 'kdsxe8t4'),
+    ('sefasaukello-6489@yopmail.com', 'kdsxe8t4'),
+    ('bofrocresuha-6968@yopmail.com', 'kdsxe8t4'),
+    ('gejoppaurufri-3677@yopmail.com', 'kdsxe8t4'),
+    ('toifocauvauzu-2252@yopmail.com', 'kdsxe8t4'),
+]
+
+for u in users:
+    with FbUsers.session() as session:
+        user = FbUsers(login=u[0],
+                       password=u[1])
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+
+
