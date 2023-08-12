64ec89b6da7bb90dcf2de0ade37b985b02b4539b
Tankovod <Wotdimas@gmail.com>
2023-08-12 13:58:06 +0300

    simple commit

--- src/settings.py
+++ src/settings.py
@@ -9,8 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SETTINGS = Settings()
 
 bot = Bot(
-    # token='6306659052:AAE_CO07WXocfidvniOKJ4HloTzjaYA0QzU',  # main TOKEN
-    token='6366199783:AAF0vbIzEo3g4hO7riXs-q0dFZEOdb6_sBM',  # test TOKEn
+    token='6306659052:AAE_CO07WXocfidvniOKJ4HloTzjaYA0QzU',  # main TOKEN
+    #token='6366199783:AAF0vbIzEo3g4hO7riXs-q0dFZEOdb6_sBM',  # test TOKEn
     parse_mode=ParseMode.HTML
 )
 
